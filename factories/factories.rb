86c4b49f1f3df712b1130ebb88ea2b73bb67e149
dfucci <davfuc@gmail.com>
2014-10-12 18:39:06 +0300

    cancan and factories

---
+++ factories/factories.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+  factory :user do
+    name Faker::Name.first_name
+    email Faker::Internet.email
+    password Faker::Internet.password
+
+    factory :user_with_bike do
+      after(:create) do |user|
+        create(:bike, user:user)
+      end
+    end
+  end
+  factory :bike do
+    name "Viking"
+    description "My fav bike"
+    price 200
+    user
+  end
+
+
+end
